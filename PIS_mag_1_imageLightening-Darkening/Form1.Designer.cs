92feafc99dd3e61e438f1664faea3be50e87e82b
StelX <stel.roman@gmail.com>
2018-09-09 19:45:50 +0500

    Add files via upload

---
+++ PIS_mag_1_imageLightening-Darkening/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace PIS_mag_1_imageLightening_Darkening
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(74, 107);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(198, 156);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(327, 107);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(209, 156);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.numericUpDown1.Location = new System.Drawing.Point(571, 153);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+}
+
---
+++ PIS_mag_1_imageLightening-Darkening/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+using System.Drawing;
+
+using System.Windows.Forms;
+
+
+namespace PIS_mag_1_imageLightening_Darkening
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.BackColor = Color.AliceBlue;
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            // take dropped items  and store 
+            var droppedFiles = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+            foreach (string file in droppedFiles)
+            {
+                pictureBox1.Image = Image.FromFile(file);
+            }
+
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if(e.Data.GetDataPresent(DataFormats.FileDrop, false) == true)
+            {
+                e.Effect = DragDropEffects.All;
+            }
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+            pictureBox2.Image = Darker((Bitmap)(pictureBox1.Image), 128);
+        
+            
+
+        }
+
+        public static Bitmap Lighter(Bitmap image, int correction)
+        {
+            Bitmap bmp = new Bitmap(image);
+            Rectangle r = new Rectangle(0, 0, bmp.Width, bmp.Height);
+            var alpha = correction;
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                using (Brush cloud_brush = new SolidBrush(Color.FromArgb(alpha, Color.White)))
+                {
+                    g.FillRectangle(cloud_brush, r);
+                }
+            }
+            return bmp;
+
+        }
+
+        public static Bitmap Darker(Bitmap image, int correction)
+        {
+            Bitmap bmp = new Bitmap(image);
+            Rectangle r = new Rectangle(0, 0, bmp.Width, bmp.Height);
+            var alpha = correction;
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                using (Brush cloud_brush = new SolidBrush(Color.FromArgb(alpha, Color.Black)))
+                {
+                    g.FillRectangle(cloud_brush, r);
+                }
+            }
+            return bmp;
+
+        }
+
+    }
+}
