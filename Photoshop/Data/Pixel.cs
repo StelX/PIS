c93855d1ad457d0c0251184a1761406f263670ea
StelX <stel.roman@gmail.com>
2018-09-25 22:54:56 +0500

    Add files via upload

---
+++ Photoshop/Data/Pixel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyPhotoshop
+{
+    public class Pixel
+    {
+        public double R { get; set; }
+        public double G { get; set; }
+        public double B { get; set; }
+        public Pixel(double red, double green, double blue)
+        {
+            this.R = red;
+            this.G = green;
+            this.B = blue;
+        }
+
+    }
+}
